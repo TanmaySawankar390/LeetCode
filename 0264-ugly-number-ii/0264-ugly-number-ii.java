f9692f3f86d796c0d8d126c77a9b88ad6d18bbb7
Tanmay Sawankar <137331988+TanmaySawankar390@users.noreply.github.com>
2025-09-24 14:20:10 +0530

    Time: 2 ms (96.11%), Space: 41.4 MB (95.49%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] uglyNumbers = new int[n];
+        uglyNumbers[0] = 1; // The first ugly number is 1
+        
+        int i2 = 0, i3 = 0, i5 = 0; // Pointers for multiples of 2, 3, and 5
+        
+        int next2 = 2, next3 = 3, next5 = 5; // Initial multiples of 2, 3, and 5
+        
+        for (int i = 1; i < n; i++) {
+            int nextUgly = Math.min(next2, Math.min(next3, next5));
+            uglyNumbers[i] = nextUgly;
+            
+            if (nextUgly == next2) {
+                i2++;
+                next2 = uglyNumbers[i2] * 2;
+            }
+            if (nextUgly == next3) {
+                i3++;
+                next3 = uglyNumbers[i3] * 3;
+            }
+            if (nextUgly == next5) {
+                i5++;
+                next5 = uglyNumbers[i5] * 5;
+            }
+        }
+        
+        return uglyNumbers[n - 1];
+    }
+}
